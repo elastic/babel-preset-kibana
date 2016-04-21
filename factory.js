0329f2a34ccd4f7684981b745fb5302922206281
spalger <email@spalger.com>
2016-04-21 13:35:25 -0500

    initial commit

---
+++ factory.js
@@ -0,0 +1,21 @@
+module.exports = function (opts) {
+  opts = opts || {};
+
+  var presets = [];
+  if (opts.react) {
+    presets.push(require('babel-preset-react'));
+  }
+
+  var plugins = [].concat(
+    require('babel-preset-es2015').plugins,
+    require('babel-preset-stage-0').plugins,
+    require('babel-preset-stage-1').plugins,
+    require('babel-preset-stage-2').plugins,
+    require('babel-preset-stage-3').plugins
+  );
+
+  return {
+    presets: presets,
+    plugins: plugins,
+  }
+};
---
+++ use_inner_modules.js
@@ -0,0 +1,1 @@
+throw new Error('kibana\'s babel presets are broken into two sub-modules, /node and /webpack. You must require one of these, i.e. `require("@elastic/babel-preset-kibana/node")`');
